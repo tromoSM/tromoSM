532f12e33593017e3a45a6ca212ca8be243f356a
tromoSM <tromosm7@gmail.com>
2025-10-12 01:05:43 +0530

    Projects/SM.$PY/GUI/V0.7/begginLet-Sniff.js
    :clone :local

---
+++ Projects/SM.$PY/GUI/V0.7/begginLet-Sniff.js
@@ -0,0 +1,16 @@
+/*
+TO YO DUMBASS IN DA FUTURE 
+DO NOT REMOVE BC,
+  ERROR 67ux
+  TOS FUNC
+  TOS CLOSE FUNC
+  TOS VERSION
+  PRIVACY FUNC
+  PRIVACY CLOSE FUNC
+  IF (textahh==null)
+   */
+let $nullrnot=0;
+let $ui_RU;
+let $tos_version;
+let $ahh = "geniusAHH-67UXPY";
+let $freeloader = "geniusAHH-67UXPY";
