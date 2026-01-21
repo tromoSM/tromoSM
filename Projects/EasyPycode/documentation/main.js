179bd7cc4420a802d4ee87a5348a2058378ab8db
tromoSM <tromosm7@gmail.com>
2026-01-22 02:18:09 +0530

    Projects/EasyPycode/documentation/main.js

--- Projects/EasyPycode/documentation/main.js
+++ Projects/EasyPycode/documentation/main.js
@@ -1,18 +1,15 @@
-window.addEventListener("DOMContentLoaded",function(){
+window.addEventListener("DOMContentLoaded",async function(){
     let DEAFAULTIMGPATH='https://raw.githubusercontent.com/tromoSM/tromoSM/refs/heads/main/Projects/EasyPycode/documentation/img/'
     let RAWIMGOUT='?r=t'
     document.querySelectorAll('[fill=imgsrc').forEach(yo=>{
         yo.src=DEAFAULTIMGPATH+yo.getAttribute('src')+RAWIMGOUT
     })
 
-    function sleep(dih){
-   return new Promise(resolve=>setTimeout(resolve,dih))
-}
     document.querySelectorAll(`[click]`).forEach(yo=>{
         if(yo.getAttribute('click')=="innerhtml"){
             yo.setAttribute('click-cache',"./"+yo.innerHTML.replaceAll(" ","-").replaceAll("_","-").replaceAll('.','-'))
             yo.addEventListener('click',function(){
-            window.open(yo.innerHTML.toLowerCase().replaceAll(" ","-").replaceAll("_","-").replaceAll('.','-'),'_self')
+            window.open(yo.innerHTML.replaceAll(" ","-").replaceAll("_","-").replaceAll('.','-')+'.html','_self')
             })
         }
         yo.addEventListener('mouseenter',function(){
@@ -41,5 +38,34 @@ window.addEventListener("DOMContentLoaded",function(){
          yo.innerHTML="Example - "+yo.innerHTML
        }
     })
-    
+   function sleep(dih){
+   return new Promise(resolve=>setTimeout(resolve,dih))
+}
+if(window.location.hash!==""){
+await sleep(367)
+  document.querySelector(`header`).setAttribute("hiddenT","40")
+  document.querySelectorAll("group").forEach(dih=>{
+    dih.style.animationDelay="0ms"
+  })
+  document.querySelectorAll(`[lite="title"]`).forEach(dih=>{
+    dih.style.animationDelay="0ms"
+  })
+ document.querySelectorAll(`[main="title"]`).forEach(dih=>{
+    dih.style.animationDelay="0ms"
+  })
+}
+  window.addEventListener("scroll",function(){
+    document.querySelector(`header`).removeAttribute("hiddenT")
+  })
+   document.querySelectorAll(`[lite="title"]`).forEach(obamahavedih=>{
+    obamahavedih.id=obamahavedih.innerHTML.split(".")[1].split("(")[0].replaceAll('.',"").replaceAll("#","").replaceAll(" ","").toLowerCase()    
+    obamahavedih.addEventListener("click",async function(){
+    window.location.hash=obamahavedih.innerHTML.split(".")[1].split("(")[0].replaceAll('.',"").replaceAll("#","").replaceAll(" ","").toLowerCase()
+    if(window.location.hash!==""){
+    await sleep(567)
+    document.querySelector(`header`).setAttribute("hiddenT","40")
+    }
+   })
+ })
+
 })
