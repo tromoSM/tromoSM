15adaf9fe9f055a45a614f9fc4f733fd9e3227dd
tromoSM <tromosm7@gmail.com>
2026-06-07 18:59:13 +0530

    Add star count animation for GitHub repositories

---
+++ t/stats.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded',async function(){
+    function sleep(dih){
+        return new Promise(resolve=>setTimeout(resolve,dih))
+    }
+    let starcount=0
+    let count=document.createElement('p')
+    count.setAttribute('stats','stars')
+    await fetch("https://api.github.com/users/tromoSM/repos").then(raw=>raw.json()).then(info=>{
+        info.forEach(repo=>{
+            starcount+=repo.stargazers_count
+        })
+    })
+    document.querySelector('[tromosm="main-bg-outr"]').append(count)
+    for(let star=0;star<=starcount;star++){
+    count.innerText=`${star} Stars earned`  
+    if(star<starcount-10){
+    await sleep(10)
+    }
+    else{
+    await sleep(70)
+    }
+    }
+})
